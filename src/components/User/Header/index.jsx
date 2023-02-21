de563ff2be40c04c4fa53130ff182af543bd92f4
Huydanger <huydanger123@gmail.com>
2023-02-21 19:59:41 +0700

    ui buoi 3

--- src/components/User/Header/index.jsx
+++ src/components/User/Header/index.jsx
@@ -1,28 +1,31 @@
-// import React from 'react'
-// import { Link } from "react-router-dom";
-// import Logo from "../../../assets/Logo/logo.png"
-// import avatar from "../../../assets/Img/Huy.jpg"
-// import "./Header.scss"
-// import { MdShoppingBasket } from "react-icons/md";
-// const Headers = () => {
-//   return (
-//     <div className='container'>
-//         <div className='nav'>
-//             <img src={Logo} className="logo" alt="logo" />
-//             <nav>
-//                 <ul>
-//                     <li><a href='/'>Home</a></li>
-//                     <li><a href='/menu'>Menu</a></li>
-//                     <li><a href='/about'>About Us</a></li>
-//                     <li><a href='/service'>Service</a></li>
-//                 </ul>
-//             </nav>
-//             <div className="relative flex items-center justify-center">
-//             <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
-//             </div>
-//             <img src={avatar} alt="userProfile" className="w-10 min-w-[40px] h-10 rounded-full ml-4 cursor-pointer"></img>
-//         </div>
-//     </div>
-//   )
-// }
-// export default Headers;
+import React from 'react'
+import { Link } from "react-router-dom";
+import Logo from "../../../assets/Logo/logo.png"
+import avatar from "../../../assets/Img/Huy.jpg"
+import "./Header.scss"
+import { MdShoppingBasket, MdFavorite } from "react-icons/md";
+const Header = () => {
+  return (
+    <div className='container'>
+        <div className='nav'>
+            <img src={Logo} className="logo" alt="logo" />
+            <nav>
+                <ul>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/menu'}>Menu</Link></li>
+                    <li><Link to={'/about'}>About Us</Link></li>
+                    <li><Link to={'/service'}>Service</Link></li>
+                </ul>
+            </nav>
+            <div className="relative flex items-center justify-center">
+              <Link to={'/favorite'}><MdFavorite className="text-textColor text-2xl  cursor-pointer" /></Link>
+            </div>
+            <div className="relative flex items-center justify-center">
+              <Link to={'/shopping'}><MdShoppingBasket href='shopping' className="text-textColor text-2xl ml-5 cursor-pointer" /></Link>
+            </div>
+            <img src={avatar} alt="userProfile" className="w-10 min-w-[40px] h-10 rounded-full ml-5 cursor-pointer"></img>
+        </div>
+    </div>
+  )
+}
+export default Header;
